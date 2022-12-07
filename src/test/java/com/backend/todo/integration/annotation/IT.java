8ffa4ee9472d94c116d81c08f6f817ee4d4ffb6e
Aleksey Voronin <fearerror@yandex.ru>
2022-12-07 20:30:28 +0300

    Added testcontainers.

---
+++ src/test/java/com/backend/todo/integration/annotation/IT.java
@@ -0,0 +1,24 @@
+package com.backend.todo.integration.annotation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * TODO: comment.
+ *
+ * @author Alexey Voronin.
+ * @since 06.12.2022.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+public @interface IT {
+}
