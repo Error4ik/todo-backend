78f0c05760ba2ab6df199e70cec8c7ea4920d861
Aleksey Voronin <fearerror@yandex.ru>
2023-01-14 14:33:12 +0300

    The exception handler has been changed.

--- src/main/java/com/backend/todo/handler/RestControllerExceptionHandler.java
+++ src/main/java/com/backend/todo/handler/RestControllerExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.backend.todo.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * @author Alexey Voronin.
@@ -14,24 +17,25 @@ import org.springframework.web.server.ResponseStatusException;
  */
 @Slf4j
 @RestControllerAdvice
-public class RestControllerExceptionHandler {
+public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler
-    public ResponseError handleException(MethodArgumentNotValidException ex) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
         log.error("ex.getMessage()", ex);
         ResponseError responseError = new ResponseError(HttpStatus.BAD_REQUEST, "Validation failed for argument.");
-        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
-            responseError.getErrorMap().put(fieldError.getField(), fieldError.getDefaultMessage());
-        });
+        ex.getBindingResult().getFieldErrors().forEach(fieldError ->
+                responseError.getErrorMap().put(fieldError.getField(), fieldError.getDefaultMessage()));
 
-        return responseError;
+        return new ResponseEntity<>(responseError, HttpStatus.BAD_REQUEST);
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler
-    public ResponseError handleException(ResponseStatusException ex) {
+    public ResponseEntity<Object> handleNotFound(ResponseStatusException ex) {
         log.error("ex.getMessage()", ex);
-        return new ResponseError(HttpStatus.NOT_FOUND, "There is no object with this ID");
+        return new ResponseEntity<>(
+                new ResponseError(HttpStatus.NOT_FOUND, "There is no object with this ID"),
+                HttpStatus.NOT_FOUND);
     }
 }
